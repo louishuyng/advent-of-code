c3f405ce005ba6e56ae23be3d77c50daa5e52cd2
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-12-06 15:27:06 +0700

    day6 part 1 advent code 2024

---
+++ 2024/day6/input_reader.rb
@@ -0,0 +1,38 @@
+def read_input
+  current_position = nil
+  current_direction = nil
+  row = 0
+  col = 0
+
+  obstacles = []
+
+  # filename = 'example_input'
+  filename = 'input'
+
+  File.open(filename, 'r') do |file|
+    file.each_line.with_index do |line, index|
+      row += 1
+      obstacles[index] = []
+      col ||= line.length
+
+      line.each_char.with_index do |char, char_index|
+        obstacles[index] << char_index if char == '#'
+
+        current_position = [index, char_index] if ['^', '>', 'v', '<'].include?(char)
+
+        case char
+        when '^'
+          current_direction = :up
+        when '>'
+          current_direction = :right
+        when 'v'
+          current_direction = :down
+        when '<'
+          current_direction = :left
+        end
+      end
+    end
+  end
+
+  [row, col, current_position, current_direction, obstacles]
+end
---
+++ 2024/day6/program.rb
@@ -0,0 +1,70 @@
+require './input_reader'
+
+def program
+  row, col, current_position, current_direction, obstacles = read_input
+
+  puts "Current position: #{current_position}"
+  puts "Current direction: #{current_direction}"
+  puts "Obstacles: #{obstacles}"
+  puts "Row: #{row}"
+  puts "Col: #{col}"
+
+  steps = 0
+  has_visited = {}
+
+  until over_tank?(current_position, row, col)
+    puts "Current position: #{current_position}"
+    if face_obstacle?(next_move(current_position, current_direction), obstacles)
+      current_direction = rotate_position_90_degree(current_direction)
+    else
+      current_position = next_move(current_position, current_direction)
+
+      has_visited[current_position[0]] = [] unless has_visited[current_position[0]]
+
+      next if has_visited[current_position[0]].include?(current_position[1])
+
+      has_visited[current_position[0]] << current_position[1]
+      steps += 1
+    end
+  end
+
+  puts "Steps: #{steps}"
+end
+
+def over_tank?(current_position, row, col)
+  current_position[0].zero? \
+  || current_position[0] == row - 1 \
+  || current_position[1].zero? \
+  || current_position[1] == col - 1
+end
+
+def face_obstacle?(current_position, obstacles)
+  obstacles[current_position[0]].include?(current_position[1])
+end
+
+def next_move(current_position, current_direction)
+  next_position = current_position.dup
+  next_position[0] += next_direction(current_direction)[0]
+  next_position[1] += next_direction(current_direction)[1]
+  next_position
+end
+
+def rotate_position_90_degree(current_direction)
+  {
+    'up': 'right',
+    'right': 'down',
+    'down': 'left',
+    'left': 'up'
+  }[current_direction.to_sym]
+end
+
+def next_direction(current_direction)
+  {
+    "up": [-1, 0],
+    "right": [0, 1],
+    "down": [1, 0],
+    "left": [0, -1]
+  }[current_direction.to_sym]
+end
+
+program
