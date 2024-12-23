0f9d9a3dc7b24c11a211833043b985e82e8d5044
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-12-23 15:50:57 +0700

    day23 part 1 advent code 2024

---
+++ 2024/dayy23/program.rb
@@ -0,0 +1,61 @@
+require 'set'
+
+def program(list_connected)
+  hash_connected = parse_connected_to_hash(list_connected)
+
+  puts "Hash connected: #{hash_connected}"
+
+  set_3_connected = create_set_3_connected_from_hash(hash_connected)
+  puts "Set 3 connected: #{set_3_connected}"
+
+  # Count only set 3 connected has name starts with 't'
+  count = set_3_connected.select do |nodes|
+    nodes.any? { |n| n.start_with?('t') }
+  end
+
+  puts "Count: #{count.length}"
+end
+
+def create_set_3_connected_from_hash(hash_connected)
+  result = Set.new
+
+  hash_connected.each do |node, connected_nodes|
+    connected_nodes.each do |connected_node|
+      hash_connected.select do |s_node, s_connected_nodes|
+        next if s_node == node || s_node == connected_node
+
+        if s_connected_nodes.include?(node) && s_connected_nodes.include?(connected_node)
+          result.add([node, connected_node, s_node].sort)
+        end
+      end
+    end
+  end
+
+  result
+end
+
+def parse_connected_to_hash(list_connected)
+  hash = {}
+
+  list_connected.each do |node_a, node_b|
+    hash[node_a] ||= []
+    hash[node_b] ||= []
+
+    hash[node_a] << node_b
+    hash[node_b] << node_a
+  end
+
+  hash
+end
+
+input = File.read('input').split("\n")
+
+list_connected = input.map do |line|
+  node_a, node_b = line.split('-')
+  puts "node_a: #{node_a}, node_b: #{node_b}"
+
+  [node_a, node_b]
+end
+
+
+program(list_connected)
